9832b85bb041d3c3e954261498c7756c2b5c36a8
rubenfm77 <88337831+rubenfm77@users.noreply.github.com>
2026-08-14 07:08:24 +0200

    Add files via upload

--- data/universe.py
+++ data/universe.py
@@ -14,19 +14,27 @@ IBEX35 = [
     "SAN.MC", "BBVA.MC", "ITX.MC", "IBE.MC", "TEF.MC", "REP.MC",
     "FER.MC", "AMS.MC", "CLNX.MC", "ELE.MC", "NTGY.MC", "CABK.MC",
     "AENA.MC", "MAP.MC", "ACS.MC", "GRF.MC", "SAB.MC", "IAG.MC",
-    "COL.MC", "MRL.MC",
+    "COL.MC", "MRL.MC", "ANA.MC", "ACX.MC", "BKT.MC", "ENG.MC",
+    "FDR.MC", "IDR.MC", "LOG.MC", "MEL.MC", "PHM.MC", "PUIG.MC",
+    "RED.MC", "ROVI.MC", "SCYR.MC", "SLR.MC", "UNI.MC",
 ]
 
 EUROSTOXX_SAMPLE = [
     "ASML.AS", "SAP.DE", "SIE.DE", "ALV.DE", "MC.PA", "OR.PA",
     "TTE.PA", "AIR.PA", "SU.PA", "DTE.DE", "BAS.DE", "ADS.DE",
-    "ENEL.MI", "ISP.MI", "STLAM.MI",
+    "ENEL.MI", "ISP.MI", "STLAM.MI", "BAYN.DE", "BMW.DE", "VOW3.DE",
+    "MBG.DE", "RWE.DE", "DB1.DE", "IFX.DE", "MUV2.DE", "DPW.DE",
+    "BNP.PA", "SAN.PA", "CS.PA", "DG.PA", "RMS.PA", "KER.PA",
+    "ENI.MI", "RACE.MI", "UCG.MI", "ASM.AS", "PHIA.AS", "INGA.AS",
 ]
 
 US_SAMPLE = [
     "AAPL", "MSFT", "GOOGL", "AMZN", "NVDA", "META", "TSLA", "BRK-B",
     "JPM", "V", "UNH", "XOM", "JNJ", "PG", "MA", "HD", "COST", "AVGO",
     "ADBE", "NFLX", "CRM", "AMD", "INTC", "PEP", "KO",
+    "WMT", "BAC", "DIS", "CSCO", "ORCL", "ABBV", "MRK", "PFE",
+    "TMO", "ABT", "ACN", "MCD", "NKE", "TXN", "QCOM", "LIN",
+    "HON", "UPS", "IBM", "GE", "CAT", "BA", "GS", "MS", "SBUX", "PYPL",
 ]
 
 UNIVERSE = {
